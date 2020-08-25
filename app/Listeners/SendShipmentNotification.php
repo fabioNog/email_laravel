4c77eed82a9f9dfe68eca91a3014b17b1e424384
fabioNog <fabio.carmo@computacao.ufla.br>
2020-08-25 19:26:29 -0300

    Criando Eventos

---
+++ app/Listeners/SendShipmentNotification.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderShipped;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendShipmentNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderShipped  $event
+     * @return void
+     */
+    public function handle(OrderShipped $event)
+    {
+        info("Entrou no Home");
+        info($event->text);
+    }
+}
